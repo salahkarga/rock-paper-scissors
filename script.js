43ce69a0ab291ecfb1ccce0900a4caeeb7cd8391
salahkarga <gherabsalah0@gmail.com>
2026-04-14 23:04:52 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// function getComputerChoice(1 , 4){
+
+// }
+
+function getRandomInt(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+}
+
+console.log(getRandomInt(1, 4))
