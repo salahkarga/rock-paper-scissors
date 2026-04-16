759d5e6eec897bc246e9a0efa58d2c5d6798bbfb
salahkarga <gherabsalah0@gmail.com>
2026-04-16 22:27:36 +0100

    Add the human choice function Add a counter to count the score of the game Add a function to play the round and declare the winner Add a loop to play 5 rounds Put the playRound function and the loop inside the game function Display the winner and the final count

--- script.js
+++ script.js
@@ -1,11 +1,70 @@
-// function getComputerChoice(1 , 4){
+function getComputerChoice(){
+  const computerMove = Math.floor(Math.random() * 3)+1;
 
-// }
+  if (computerMove === 1){
+    return 'Rock';
+  }else if(computerMove === 2){
+    return 'Paper';
+  }else {
+    return 'Scissors';
+  }
 
-function getRandomInt(min, max) {
-  const minCeiled = Math.ceil(min);
-  const maxFloored = Math.floor(max);
-  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
 }
 
-console.log(getRandomInt(1, 4))
+function getHumanChoice(){
+  const choice = prompt("What is your move?");
+
+  return choice.charAt(0).toUpperCase() + choice.slice(1).toLowerCase();
+}
+
+
+function playGame(){
+  let humanScore = 0;
+  let computerScore = 0;
+  
+  function playRound(humanChoice, computerChoice){
+    if (humanChoice === 'Rock'){
+      if (computerChoice === 'Rock'){
+        return 'its a Draw !';
+      }else if (computerChoice === 'Paper'){
+        computerScore++;
+        return 'You Lose !';     
+      }else if (computerChoice === 'Scissors'){
+        humanScore++
+        return 'You Win !';
+        }
+
+    }else if (humanChoice === 'Paper'){
+      if (computerChoice === 'Paper'){
+        return 'its a Draw !';
+      }else if (computerChoice === 'Scissors'){
+        computerScore++;
+        return 'You Lose !';
+          
+      }else if (computerChoice === 'Rock'){
+        humanScore++
+        return'You Win !';
+        }
+
+    }else if (humanChoice === 'Scissors'){
+      if (computerChoice === 'Scissors'){
+        return'its a Draw !';
+      }else if (computerChoice === 'Rock'){
+        computerScore++;
+        return'You Lose !'; 
+      }else if (computerChoice === 'Paper'){
+        humanScore++;
+        return'You Win !';          
+        }
+    }
+  }
+  for(let i=0; i<5; i++){
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+
+    console.log(playRound(humanSelection,computerSelection))
+
+    console.log(humanScore, computerScore);
+  }
+}
+console.log(playGame())
